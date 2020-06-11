e12f4d04ce5155e8bb7cefe17066dc33e50b9103
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-06-11 19:19:04 +0300

    регистрация

---
+++ account/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(verbose_name = "email", max_length = 60, unique = True)
+    username = models.CharField(max_length = 30, unique = True)
+    date_joined = models.DateTimeField(verbose_name = "date joined", auto_now_add = True)
+    last_login = models.DateTimeField(verbose_name = "last_login", auto_now = True)
+    is_admin = models.BooleanField(default = False)
+    avatar = models.ImageField(upload_to="user_avatars/%Y/%m/%d", blank=True)
+
+    def get_url(self):
+        try:
+            return self.avatar.url
+        except ValueError:
+            return None
+
+    def __init__(self, email, username, admin = False):
+        self.email = email
+        self.username = username
+        self.is_admin = admin
