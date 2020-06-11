9dd67d17994289c425041ec1658ee82a22e76120
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-06-11 22:38:59 +0300

    123

--- account/api/views.py
+++ account/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from django.views import View
-from rest_framework.respone import Response
+from rest_framework.response import Response
 from account.api.serializers import RegistrationSerializer
 
 class RegistrationView(View):
